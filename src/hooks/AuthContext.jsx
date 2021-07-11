188e6740588406aa0947c97e0cdda93e1ae430e9
Leonardo Sanches <sleonardogabriel@gmail.com>
2021-07-11 20:38:12 -0300

    Finalização do processo de login

--- src/hooks/AuthContext.jsx
+++ src/hooks/AuthContext.jsx
@@ -4,16 +4,30 @@ import api from '../services/api';
 const AuthContext = createContext({});
 
 export function AuthProvider({ children }) {
-  const [user, setUser] = useState();
+  const [user, setUser] = useState(() => {
+    const userStorage = JSON.parse(localStorage.getItem('@Ioasys:User'));
+
+    if (!userStorage) return undefined;
+
+    api.defaults.headers.Authorization = `Bearer ${userStorage?.token}`;
+
+    return userStorage;
+  });
 
   function login(userlogin, token, refreshToken) {
-    setUser({ ...userlogin, token, refreshToken });
+    const loginUser = { ...userlogin, token, refreshToken };
+
+    localStorage.setItem('@Ioasys:User', JSON.stringify(loginUser));
+
+    setUser(loginUser);
 
     api.defaults.headers.Authorization = `Bearer ${token}`;
   }
 
   function logout() {
     setUser(undefined);
+
+    localStorage.removeItem('@Ioasys:User');
   }
 
   return (
